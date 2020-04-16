4426e45eb36594ced962b7ee868cc536f65be7fc
ogata alloysius kebaso <Loykebaso@gmail.com>
2020-04-16 16:18:47 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>my side bar</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+</head>
+<body class="section">
+	<input type="checkbox" id="check">
+	<label for="check">
+		<i class="fas fa-bars" id="btn"></i>
+		<i class="fas fa-times" id="cancel"></i>
+	</label>
+	<div class="sidebar">
+		<header>my app</header>
+		<ul>
+			<li><a href=""><i class="fas fa-qrcode"></i>DashBoard</a></li>
+			<li><a href=""><i class="fas fa-link"></i>Shortcuts</a></li>
+			<li><a href=""><i class="fas fa-stream"></i>Overview</a></li>
+			<li><a href=""><i class="far fa-adjust"></i>Events</a></li>
+			<li><a href=""><i class="far fa-question-circle"></i>About</a></li>
+			<li><a href=""><i class="fas fa-sliders-h"></i>Services</a></li>
+			<li><a href=""><i class="far fa-envelope"></i>Contacts</a></li>
+		</ul>
+	</div>
+	<section>
+		<h2>home</h2>
+		
+	</section>
+
+</body>
+</html>
